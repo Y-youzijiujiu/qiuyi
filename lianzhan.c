ce5614934cd8f71004370c72e96c9190195600f5
Y-youzijiujiu <74645289+Y-youzijiujiu@users.noreply.github.com>
2020-12-03 20:12:34 +0800

    Add files via upload

--- lianzhan.c
+++ lianzhan.c
@@ -4,47 +4,6 @@
 #include<string.h>
 
 
-typedef int dataType;
-typedef struct node {
-	dataType data;
-	struct node *next;
-} LinkStack;
-
-//初始化链栈
-LinkStack* InitStack() {
-	LinkStack *t = (LinkStack*)malloc(sizeof(LinkStack));
-	t->next = NULL;
-	return t;
-}
-
-//判断栈是否为空
-int Empty(LinkStack *s) {
-	return s->next == NULL;
-}
-
-//元素x进栈
-void Push(LinkStack *s,dataType x) {
-	struct node *t = (struct node*)malloc(sizeof(struct node));
-	t->data = x;
-	t->next = s->next;
-	s->next = t;
-}
-
-//出栈
-void Pop(LinkStack *s) {
-	if (Empty(s))  exit(1);
-       struct node *p = s->next;
-	s->next = p->next;
-	free(p);
-}
-
-//取c栈顶元素的值
-dataType GetTop(LinkStack *s) {
-	return s->next->data;
-}
-
-
-
 
 int priority(char x) {
 	switch (x) {
